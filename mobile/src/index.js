9da902bf269912695a7ce8e96a527dc49fd67efc
Igor Martins <m.oliverunb@gmail.com>
2019-02-20 10:36:41 -0300

    Initial Commit

---
+++ mobile/src/index.js
@@ -0,0 +1,10 @@
+import React from "React";
+import { YellowBox } from "react-native";
+
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
+
+import Routes from "./routes";
+
+const App = () => <Routes />;
+
+export default App;
